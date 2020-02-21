d3bb73aaaca03c03887691f88d7b4b58fed7c9f6
Anton Nikolaev <regwebghost@yandex.ru>
2020-02-21 13:33:45 +0300

    Add code

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from . import models
+from .api import items, users
+from .database import engine
+
+app = FastAPI()
+
+app.include_router(users.router, tags=["users"])
+app.include_router(items.router, tags=["items"])
+
+
+@app.on_event("startup")
+def startup_event():
+    models.Base.metadata.create_all(bind=engine)
---
+++ app/schemas.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: str = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    # id: int = Field(..., gt=5)
+    # email: str = Field(..., max_length=30)
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
